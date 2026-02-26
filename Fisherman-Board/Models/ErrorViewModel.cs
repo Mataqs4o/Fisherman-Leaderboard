6647043163025ea6e409fe2cbc5e4260e635e4ae
Matias <Admin@DESKTOP-D01KSMT>
2026-02-26 19:56:21 +0200

    Add project files.

---
+++ Fisherman-Board/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fisherman_Board.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
