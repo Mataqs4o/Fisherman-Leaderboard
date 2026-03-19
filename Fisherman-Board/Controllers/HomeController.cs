297732547c540bbe75c18a3a95f1128008ba2ea8
sanch08 <stanislavmikov1@gmail.com>
2026-03-19 12:35:53 +0200

    Написах коментари на HomeController.cs

--- Fisherman-Board/Controllers/HomeController.cs
+++ Fisherman-Board/Controllers/HomeController.cs
@@ -1,32 +1,35 @@
-using System.Diagnostics;
-using Fisherman_Board.Models;
-using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics; // Използва се за работа с процеси и диагностика
+using Fisherman_Board.Models; // Импорт на моделите от проекта
+using Microsoft.AspNetCore.Mvc; // Основни класове за MVC
 
 namespace Fisherman_Board.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller // Главен контролер за приложението
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger; // Логър за запис на събития и грешки
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger) // Конструктор с dependency injection
         {
-            _logger = logger;
+            _logger = logger; // Присвояване на подадения логър
         }
 
-        public IActionResult Index()
+        public IActionResult Index() // Метод за началната страница
         {
-            return View();
+            return View(); // Връща изгледа Index
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privacy() // Метод за страницата Privacy
         {
-            return View();
+            return View(); // Връща изгледа Privacy
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)] // Забранява кеширането
+        public IActionResult Error() // Метод за обработка на грешки
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier // Взима текущото ID на заявката
+            });
         }
     }
 }
