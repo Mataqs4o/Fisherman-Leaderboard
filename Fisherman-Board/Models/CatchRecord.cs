adea6c850f661ab87c822ee48faff77fd9dd6faa
Vrab1ca <niikoola08@gmail.com>
2026-06-25 00:08:54 +0300

    add to choose fisherman

--- Fisherman-Board/Models/CatchRecord.cs
+++ Fisherman-Board/Models/CatchRecord.cs
@@ -7,6 +7,13 @@ public class CatchRecord
 {
     public int Id { get; set; }
 
+    [Range(1, int.MaxValue, ErrorMessage = "Изберете рибар.")]
+    [Display(Name = "Рибар")]
+    public int PersonId { get; set; }
+
+    [ValidateNever]
+    public Person Person { get; set; } = null!;
+
     [Range(1, int.MaxValue, ErrorMessage = "Изберете излет.")]
     [Display(Name = "Излет")]
     public int FishingTripId { get; set; }
